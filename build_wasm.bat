3335f3db6f27320fc39b415c6f41b118e60d12e5
Rudra <rudra@example.com>
2026-06-12 00:32:59 +0530

    Fix: Recompile Wasm without EXPORT_ES6 to fix 'SyntaxError: Cannot use import statement outside a module' on frontend

--- build_wasm.bat
+++ build_wasm.bat
@@ -1,6 +1,6 @@
 @echo off
 call .\emsdk\emsdk_env.bat
-emcc Board.cpp BoardAI.cpp AI.cpp Square.cpp WasmBindings.cpp -o frontend/public/chess_module.js -s EXPORT_ES6=1 -s MODULARIZE=1 -s EXPORT_NAME="chess_module" -s ENVIRONMENT=web -s ALLOW_MEMORY_GROWTH=1 -O3 --bind
+emcc Board.cpp BoardAI.cpp AI.cpp Square.cpp WasmBindings.cpp -o frontend/public/chess_module.js -s MODULARIZE=1 -s EXPORT_NAME="chess_module" -s ENVIRONMENT=web -s ALLOW_MEMORY_GROWTH=1 -O3 --bind
 cd frontend
 npm run build
 git add ..
